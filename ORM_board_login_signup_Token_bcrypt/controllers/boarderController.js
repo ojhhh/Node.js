468a95877937035a7ac9bc5cd87a949e551a89d0
ojhhh <winnel@naver.com>
2023-05-22 19:21:56 +0900

    코드 설명 추가

--- ORM_board_login_signup_Token_bcrypt/controllers/boarderController.js
+++ ORM_board_login_signup_Token_bcrypt/controllers/boarderController.js
@@ -32,12 +32,16 @@ exports.createBorder = async (req, res) => {
 };
 
 exports.borderView = (req, res) => {
+  // where 쓸때 {} 빼먹지 말자
   User.findOne({
     where: { id: req.params.id },
     // Post 모델을 참조
     include: [{ model: Post }],
   }).then((e) => {
     // map 함수 화살표 함수뒤에 중괄호를 생략하면 바로 값을 반환
+    // -> map을 통해 타입이 변경되는 건줄 알았는데 아니였음
+    // -> Posts 객체안에 post 오브젝트가 여러개의 객체를 담고 있음
+    // -> 그 안에 있는 dataValues의 내용만 다시 Posts에 담음 (불필요한 내용은 빼고 다시 저장하는 식)
     e.dataValues.Posts = e.dataValues.Posts.map((i) => i.dataValues);
     const Posts = e.dataValues;
     res.render("border", { data: Posts });
--- ORM_board_login_signup_Token_bcrypt/middleware/login.js
+++ ORM_board_login_signup_Token_bcrypt/middleware/login.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 
+// 로그인 검증이 필요한 곳에 사용하기 위해 따로 만듬
 exports.isLogin = (req, res, next) => {
   const { access_token } = req.session;
   jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY, (err, acc_decoded) => {
