603c214b761203e7d6055fa3c8a6c6f07eace025
ojhhh <winnel@naver.com>
2024-02-13 21:29:51 +0900

    google login api 로그인 까지 확인

--- bolier-plate/google-login/app.js
+++ bolier-plate/google-login/app.js
@@ -1,81 +1,55 @@
 require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-
+const axios = require("axios");
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 세션 설정
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-// Passport 초기화
-app.use(passport.initialize());
-// Passport 세션 사용 설정
-app.use(passport.session());
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      // console.log("accessToken : ", accessToken);
-      // console.log("refreshToken : ", refreshToken);
-      // console.log("profile : ", profile);
-      return cb(null, profile);
-    }
-  )
-);
-
-// 사용자 세션에 저장
-passport.serializeUser((user, done) => {
-  done(null, user);
+app.get("/auth/google", (req, res) => {
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&scope=profile email&access_type=offline`;
+  res.redirect(authUrl);
 });
 
-// 세션에서 사용자 정보 추출
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
-});
-
-app.get("/", (req, res) => {
+app.get("/auth/google/callback", async (req, res) => {
+  const code = req.query.code;
   try {
-    // console.log(req);
+    const { data } = await axios.post(
+      `https://oauth2.googleapis.com/token`,
+      {
+        code,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        redirect_uri: process.env.REDIRECT_URI,
+        grant_type: "authorization_code",
+      },
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
 
-    const user = req.user;
+    console.log("data.access_token  : ", data.access_token);
 
-    res.json(user);
+    console.log("data.refresh_token  : ", data.refresh_token);
+
+    const userInfo = await axios.get(
+      `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${data.access_token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${data.access_token}`,
+        },
+      }
+    );
+
+    // console.log("userInfo : ", userInfo);
+
+    console.log(userInfo.data);
+    res.send(userInfo.data);
   } catch (error) {
     console.error(error);
+    res.status(500).send("Authentication error");
   }
 });
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  function (req, res) {
-    // 성공적인 인증 후 리다이렉션
-    res.redirect("/");
-  }
-);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server `);
 });
